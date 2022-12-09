fbfff3d6d53fda08838d92449b623af6691dbe3d
Somers B Matthews <somersbmatthews@gmail.com>
2022-12-09 10:48:27 -0600

    initial commit

---
+++ src/main/java/com/revature/diningphilosophers/DiningPhilosophersApplication.java
@@ -0,0 +1,13 @@
+package com.revature.diningphilosophers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningPhilosophersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningPhilosophersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/diningphilosophers/DiningPhilosophersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.diningphilosophers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningPhilosophersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
